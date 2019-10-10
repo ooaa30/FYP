22caf8ac75af67bab8e6e721ec9a58055702abba
Nathan <natlivsey@gmail.com>
2019-10-10 16:17:58 +0100

    Complete candidate action, moves unit towards the "middle" hex

--- code/wesnoth/add-ons/test_addon/lua/walk.lua
+++ code/wesnoth/add-ons/test_addon/lua/walk.lua
@@ -12,10 +12,32 @@ end
 
 local ca_return_unit = {}
 
-function ca_return_unit:evaluation(ai,cfg)
+function ca_return_unit:evaluation(cfg)
   local unit = get_unit(cfg)
   if unit then
     return 250000
   end
   return 0
+end
+
+function ca_return_unit:execution(cfg)
+  local width, height = wesnoth.get_map_size()
+  local targetX = (width - width % 2) / 2
+  local targetY = (height - height % 2) / 2
+  local unit = get_unit(cfg)
+
+  -- If targt hex is occupied find a vacant hex
+  if(unit.x ~= targetX) or (unit.x ~= targetY) then
+    targetX,targetY = wesnoth.find_vacant_tile(targetX,targetY, unit)
+  end
+
+  --find closest hex the unit can move in one turn
+  local next_hop = AH.next_hop(unit, targetX,targetY)
+  if(not next_hop) then next_hop = {unit.x,unit.y} end
+
+  if ((next_hop[1] ~= unit.x) or (next_hop[2] ~= unit.y)) then
+    ai.move_full(unit, next_hop[1], next_hop[2])
+  else
+    ai.stopunit_moves(unit)
+  end
 end
